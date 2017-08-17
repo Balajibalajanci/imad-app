ec2dfbf8ee7b4ca694d8ba99acbc295758e928d0
Balajibalajanci <30999972+Balajibalajanci@users.noreply.github.com>
2017-08-17 08:51:15 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='THEORY OF COMPUTATION';
+//maove image
+var image=document.getElementById('madi');
+image.onclick=function(){
+     image.style.marginLeft='100px';  
+};
