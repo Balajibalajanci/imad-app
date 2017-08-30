28f01b3ea3a6d51fcc011533c7ff272d494b80f9
Balajibalajanci <30999972+Balajibalajanci@users.noreply.github.com>
2017-08-30 07:25:05 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,1 @@
 console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='THEORY OF COMPUTATION';
-//maove image
-var image=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginleft + 10;
-    image.style.marginLeft =marginLeft + 'px';
-}
-
-
-
-image.onclick=function(){
-    
-    var interval=setInterval(moveRight,100);
-      
-};
