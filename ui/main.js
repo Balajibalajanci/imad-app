6a1cf064e367920ceca0f18921f6dc3276ee0f44
Balajibalajanci <30999972+Balajibalajanci@users.noreply.github.com>
2017-08-30 08:24:43 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,24 @@ console.log('Loaded!');
 var button =document.getElementById("counter");
 var counter=0;
 button.onclick=function() {
-    
-    
-    
-    
-    
-    
-    counter =counter+1;
-    var span=document.getElementById("count");
+    var request =new XMLHttprequest();
+    request.onreadystatechange=function()
+    { 
+        if(request.readystate=== XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                  var span=document.getElementById("count");
     span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open("http://blgsathiyan.imad.hasura-app.io/counter");
+    request.send(null);
+    
+    
+    
+   
+  
 };
